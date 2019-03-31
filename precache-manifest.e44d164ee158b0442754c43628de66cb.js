7b9c660efd2db5983deae934184ef0c0b92d73a1
Instinktaz <tomas.paskovskij@gmail.com>
2019-03-31 19:34:49 +0300

    Updates

---
+++ precache-manifest.e44d164ee158b0442754c43628de66cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TomasInstinktaz/Image-api-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "951c2859d33608ef454e3bcb3e0d50b2",
+    "url": "/TomasInstinktaz/Image-api-app/static/media/loader.951c2859.gif"
+  },
+  {
+    "revision": "726ca2d32e4692ae16b0",
+    "url": "/TomasInstinktaz/Image-api-app/static/js/runtime~main.56eca570.js"
+  },
+  {
+    "revision": "be3715344b8f13f6a128",
+    "url": "/TomasInstinktaz/Image-api-app/static/js/main.758ac423.chunk.js"
+  },
+  {
+    "revision": "fcf99d4710d47b39927a",
+    "url": "/TomasInstinktaz/Image-api-app/static/js/2.d46f1d8b.chunk.js"
+  },
+  {
+    "revision": "be3715344b8f13f6a128",
+    "url": "/TomasInstinktaz/Image-api-app/static/css/main.d2bfa563.chunk.css"
+  },
+  {
+    "revision": "fcf99d4710d47b39927a",
+    "url": "/TomasInstinktaz/Image-api-app/static/css/2.93f0a5d2.chunk.css"
+  },
+  {
+    "revision": "20c6cd36d88bbb5d6f169c19224aae46",
+    "url": "/TomasInstinktaz/Image-api-app/index.html"
+  }
+];
